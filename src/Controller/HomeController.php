11fcb8fb13d38c6fbe40f2a333affd4eaf471f67
GillesFromGuinot <glevy@guinot.asso.fr>
2023-02-21 11:15:25 +0100

    21.02.2023

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,7 +9,6 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="app_home")
      * @Route("/", name="app_home")
      */
     public function index(): Response
